5c45a661e1d11384082f06bee0307b7f744ae462
wenxich <wenxi.c.chen@gmail.com>
2022-07-19 18:54:01 -0400

    Made the scene visible.

--- main.js
+++ main.js
@@ -9,3 +9,27 @@ const renderer = new THREE.WebGLRenderer();
 console.log(scene);
 console.log(camera);
 console.log(renderer);
+
+renderer.setSize(innerWidth, innerHeight);
+
+document.body.appendChild(renderer.domElement); //insert this into body
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1); //width, height, depth
+
+//won't see anything unless you create a mesh, so create a mesh + object
+const material = new THREE.MeshBasicMaterial({color:0x00ff00});
+
+console.log(boxGeometry);
+console.log(material);
+
+const mesh = new THREE.Mesh(boxGeometry, material);
+console.log(mesh);
+
+//adds the mesh to the scene
+scene.add(mesh);
+
+//makes what's in front of the black screen visible
+camera.position.z = 5;
+
+//renders it all
+renderer.render(scene, camera);
