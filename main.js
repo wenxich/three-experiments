9176d6d951f8f540a75aac2093e3644fedcb9088
wenxich <wenxi.c.chen@gmail.com>
2022-07-19 22:19:10 -0400

    Modify plane using dat.gui.

--- main.js
+++ main.js
@@ -1,6 +1,55 @@
 import './style.css'
 
 import * as THREE from 'three';
+import * as dat from 'dat.gui';
+
+//DAT.GUI SLIDERS
+
+//create a gui object to use dat.gui
+const gui = new dat.GUI();
+const world = {
+    plane: {
+        width: 10,
+        height: 10,
+        widthSegments: 10,
+        heightSegments: 10
+    }
+}
+
+//dat.gui - WIDTH
+gui.add(world.plane, 'width', 1, 20).onChange(generatePlane);
+
+//dat.gui - HEIGHT
+gui.add(world.plane, 'height', 1, 20).onChange(generatePlane);
+
+//dat.gui - WIDTH
+gui.add(world.plane, 'widthSegments', 1, 20).onChange(generatePlane);
+
+//dat.gui - HEIGHT
+gui.add(world.plane, 'heightSegments', 1, 20).onChange(generatePlane);
+
+function generatePlane() {
+    planeMesh.geometry.dispose();
+    planeMesh.geometry = new THREE.PlaneGeometry(
+        world.plane.width,
+        world.plane.height,
+        world.plane.widthSegments,
+        world.plane.heightSegments
+    );
+
+    const {array} = planeMesh.geometry.attributes.position;
+
+    for (let i = 0; i < array.length; i+=3) {
+        const x = array[i];
+        const y = array[i + 1];
+        const z = array[i + 2];
+
+        //randomizing z position to add depth/jaggedness to plane
+        array[i + 2] = z + Math.random();
+    }
+}
+
+//THREE.JS SCENE
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth/innerHeight, 0.1, 1000); //field of view (degrees), aspect ratio (fraction) + 2 clipping planes
@@ -13,19 +62,6 @@ renderer.setPixelRatio(devicePixelRatio);
 
 document.body.appendChild(renderer.domElement); //insert this into body
 
-//parameters: width, height, depth, widthSegments, heightSegments, depthSegments
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-
-//won't see anything unless you create a mesh, so create a mesh + object
-
-//colors: https://libxlsxwriter.github.io/working_with_colors.html
-const boxMaterial = new THREE.MeshBasicMaterial(
-    {color:0xC0C0C0});
-const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-
-//adds the mesh to the scene
-scene.add(boxMesh);
-
 //makes what's in front of the black screen visible
 camera.position.z = 5;
 
@@ -33,9 +69,21 @@ camera.position.z = 5;
 //parameters: width, height, widthSegments, heightSegments
 const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
 
-//add lighting to plane
+//won't see anything unless you create a mesh, so create a mesh + object
+
+//MeshPhongMaterial adds lighting to plane in comparison to MeshBasicMaterial
 const planeMaterial = new THREE.MeshPhongMaterial(
-    {color:0x800000, side: THREE.DoubleSide});
+    {
+        //colors: https://libxlsxwriter.github.io/working_with_colors.html
+        color:0x800000,
+
+        side: THREE.DoubleSide,
+
+        //make vertices visible
+        flatShading: THREE.FlatShading
+    });
+
+//adds the mesh to the scene
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
 
 scene.add(planeMesh);
@@ -46,8 +94,11 @@ const {array} = planeMesh.geometry.attributes.position;
 
 for (let i = 0; i < array.length; i+=3) {
     const x = array[i];
-    const y = array[i+1];
-    const z = array[i+2];
+    const y = array[i + 1];
+    const z = array[i + 2];
+
+    //randomizing z position to add depth/jaggedness to plane
+    array[i + 2] = z + Math.random();
 }
 
 //parameters: color, intensity
@@ -64,8 +115,6 @@ renderer.render(scene, camera);
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    boxMesh.rotation.x += 0.01;
-    boxMesh.rotation.y += 0.01;
     planeMesh.rotation.x += 0.01;
 }
 
